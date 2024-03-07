0db3b2c0df6ef66c5306c2c419a8848dbf8a2a57
Jan Fässler <mail@janfaessler.ch>
2024-03-07 01:01:51 +0100

    add first unit tests

--- SwiftChessTests/SwiftChessTests.swift
+++ SwiftChessTests/SwiftChessTests.swift
@@ -1,21 +1,16 @@
 //
-//  BoardTests.swift
-//  SwiftChess
+//  SwiftChessTests.swift
+//  SwiftChessTests
 //
-//  Created by Jan Fässler on 30.12.21.
+//  Created by Jan Fässler on 06.03.2024.
 //
 
 import XCTest
-@testable import Board
 
-class BoardTests: XCTestCase {
+final class SwiftChessTests: XCTestCase {
 
-    private let testee:Board
-    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        super.setUpWithError()
-        testee = Board()
     }
 
     override func tearDownWithError() throws {
@@ -28,13 +23,11 @@ class BoardTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        board.load
-        board.addFigures()
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
