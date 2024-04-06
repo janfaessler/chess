e4b8d4127c5d00623b4e8ddb7cd5855152b8f607
Jan Fässler <mail@janfaessler.ch>
2024-04-07 01:52:06 +0200

    improve controll view

---
+++ Shared/Views/Control/ControlButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ControlButtonView.swift
+//  SwiftChess
+//
+//  Created by Jan Fässler on 07.04.2024.
+//
+
+import SwiftUI
+
+struct ControlButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlButtonView()
+}
---
+++ Shared/Views/Control/MoveListView.swift
@@ -0,0 +1,18 @@
+//
+//  MoveListView.swift
+//  SwiftChess
+//
+//  Created by Jan Fässler on 07.04.2024.
+//
+
+import SwiftUI
+
+struct MoveListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoveListView()
+}
