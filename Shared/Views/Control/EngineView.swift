ea97d6fb7906f8019f543bc9b9deb2020cacb32c
Jan Fässler <mail@janfaessler.ch>
2024-04-15 15:45:17 +0200

    add scroll view

--- Shared/Views/Control/EngineView.swift
+++ Shared/Views/Control/EngineView.swift
@@ -8,6 +8,7 @@ struct EngineView: View {
         VStack(alignment: .leading) {
             ForEach(lines) { x in
                 Text("\(x.score) \(x.line)")
+                    .lineLimit(1)
             }
         }
         .padding(5)
--- Shared/Views/Control/MoveListView.swift
+++ Shared/Views/Control/MoveListView.swift
@@ -3,22 +3,31 @@ import SwiftUI
 struct MoveListView: View {
     
     @ObservedObject var model:ControlModel
-    
+    @Namespace var bottomID
+
     var body: some View {
 
-        LazyVGrid(columns: model.moveListColumns, alignment: .leading) {
-                
-                ForEach(Array(model.realMoves.enumerated()), id: \.element.id) { index, move in
-                    if model.isNewRow(index) {
-                        Text(model.getRowDescriptionText(index))
-                    }
-                    
-                    MoveView(model: model, index: index) {
-                        model.goToMove(index + 1)
-                    }
+        ScrollView {
+            ScrollViewReader { scrollView in
+                LazyVGrid(columns: model.moveListColumns, alignment: .leading) {
+                        ForEach(Array(model.realMoves.enumerated()), id: \.element.id) { index, move in
+                            if model.isNewRow(index) {
+                                Text(model.getRowDescriptionText(index))
+                            }
+                            
+                            MoveView(model: model, index: index) {
+                                model.goToMove(index + 1)
+                            }
+                        }
+                        
+                        Spacer().id(bottomID)
                 }
-                Spacer()
+                .padding(10)
+                .onChange(of: model.realMoves.count) {
+                    scrollView.scrollTo(bottomID, anchor: .top)
+                }
+            }
         }
-        .padding(10)
+
     }
 }
