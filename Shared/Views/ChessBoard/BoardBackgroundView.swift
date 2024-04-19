8dfa9b702991ac92e7078d9fbce2d8b2737881b1
Jan Fässler <mail@janfaessler.ch>
2024-04-19 18:31:38 +0200

    add fieldnames

--- Shared/Views/ChessBoard/BoardBackgroundView.swift
+++ Shared/Views/ChessBoard/BoardBackgroundView.swift
@@ -16,24 +16,41 @@ struct BoardBackgroundView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0.0) {
-            ForEach(1...8, id: \.self) { row in
-                HStack(spacing: 0.0) {
-                    ForEach(1...8, id: \.self) { file in
-                        self.getRectangle(row: row, file: file, color: getColor(row: row, file: file))
+            VStack(spacing: 0.0) {
+                ForEach(1...8, id: \.self) { row in
+                    HStack(spacing: 0.0) {
+                        ForEach(1...8, id: \.self) { file in
+                            ZStack {
+                                Rectangle()
+                                    .fill(getColor(row: row, file: file))
+                                    .frame(width: fieldSize, height: fieldSize)
+                                    .onTapGesture { onTab(row: row, file: file) }
+                                
+                                if row == 8 {
+                                    Text(getFileName(file))
+                                        .fontWeight(.bold)
+                                        .font(.largeTitle)
+                                        .foregroundStyle(getTextColor(row: row, file: file))
+                                        .padding(3)
+                                        .frame(width: fieldSize, height: fieldSize, alignment: .bottomTrailing)
+                                }
+                                
+                                if file == 1 {
+                                    Text(getRowName(row))
+                                        .fontWeight(.bold)
+                                        .font(.largeTitle)
+                                        .foregroundStyle(getTextColor(row: row, file: file))
+                                        .padding(3)
+                                        .frame(width: fieldSize, height: fieldSize, alignment: .topLeading)
+                                }
+                            }
+                        }
                     }
                 }
             }
-        }
     }
     
-    func getRectangle(row: Int,file: Int, color:Color) -> some View {
-        return Rectangle()
-            .fill(getColor(row: row, file: file))
-            .frame(width: fieldSize, height: fieldSize)
-            .onTapGesture { onTab(row: row, file: file) }
 
-    }
     
     func onTab(row: Int, file:Int) {
         try? model.moveFocusFigureTo(row: (9-row), file: file)
@@ -44,4 +61,17 @@ struct BoardBackgroundView: View {
         return odd ? lightColor : darkColor
     }
     
+    func getTextColor(row: Int, file: Int) -> Color {
+        let odd = (row + file) % 2 == 0
+        return odd ? darkColor : lightColor
+    }
+    
+    func getFileName(_ file:Int) -> String {
+        let field = Field(row: 1, file: file)
+        return field.getFileName()
+    }
+    
+    func getRowName(_ row:Int) -> String {
+        "\(9-row)"
+    }
 }
