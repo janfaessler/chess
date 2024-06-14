9c0b0e9931165b72b807da90577569c79d5de9eb
Jan Fässler <mail@janfaessler.ch>
2024-06-14 16:38:39 +0200

    display all variations

--- Shared/Control/MoveList/Views/VariationView.swift
+++ Shared/Control/MoveList/Views/VariationView.swift
@@ -11,7 +11,8 @@ struct VariationView: View {
                     Text(variation)
                 }
             }
-        }   
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
     }
     
     func hasVariations(_ container:MoveContainer?) -> Bool {
@@ -21,8 +22,22 @@ struct VariationView: View {
     func getVariations(_ container:MoveContainer?) -> [String] {
         var variations:[String] = []
         for v in container!.variations.values {
-            variations.append(Array(v).map({ "\($0.moveNumber) \($0.white?.move.info() ?? "..."),\($0.black?.move.info() ?? "")"  }).joined(separator: " "))
+            variations.append(Array(v).map({ "\($0.moveNumber). \(getMove($0.white))\($0.white == nil ? "..." : "")\($0.black != nil ? ", " : "")\(getMove($0.black))"  }).joined(separator: " "))
         }
         return variations
+    }
+    
+    func getMove(_ container:MoveContainer?) -> String {
+        guard let moveContainer = container else { return "" }
+        var moveString:String = moveContainer.move.info()
+        
+        if moveContainer.variations.count > 0 {
+            let variations = getVariations(moveContainer)
+            for variation in variations {
+                moveString.append(" (\(variation))")
+            }
+        }
+        
+        return moveString
     }
 }
