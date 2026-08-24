99184927ab0a4ff3e8be6a107b239b21ed2d6347
Jan Fässler <mail@janfaessler.ch>
2026-08-24 15:44:15 +0200

    some improvements

--- Shared/DataManagment/GameCollectionRepository.swift
+++ Shared/DataManagment/GameCollectionRepository.swift
@@ -5,3 +5,10 @@ protocol GameCollectionRepository {
     func load() throws -> [GameCollection]
     func save(_ collections: [GameCollection]) throws
 }
+
+extension GameCollectionRepository {
+    func load() async throws -> [GameCollection] {
+        let syncLoad: () throws -> [GameCollection] = self.load
+        return try syncLoad()
+    }
+}
--- Shared/DataManagment/Structs/GameCollection.swift
+++ Shared/DataManagment/Structs/GameCollection.swift
@@ -4,7 +4,7 @@ struct GameCollection: Identifiable, Hashable {
     let id: UUID
     var name: String
     var expanded: Bool
-    var games: [GameData] = []
+    private(set) var games: [GameData] = []
 
     init(id: UUID = UUID(), name: String, expanded: Bool, games: [GameData] = []) {
         self.id = id
